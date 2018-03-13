f36c11fb90b2e226f656b3aee4730ca3730fc411
Ian James <injms@users.noreply.github.com>
2018-03-13 20:36:52 +0000

    Removed promises polyfill.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,27 @@ module.exports = {
 	},
 	plugins: [
 		`gatsby-plugin-react-helmet`,
-		`gatsby-plugin-sass`
+		`gatsby-plugin-sass`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `inj.ms`,
+				short_name: `inj.ms`,
+				start_url: `/`,
+				background_color: `#eeeeee`,
+				theme_color: `#f9bf3b`,
+				display: `minimal-ui`
+			},
+		},
+		`gatsby-plugin-offline`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `posts`,
+				path: `${__dirname}/src/posts`,
+			}
+		},
+		`gatsby-transformer-remark`
 	],
+	polyfill: false
 };
