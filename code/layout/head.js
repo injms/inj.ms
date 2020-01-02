fa469b3aa0358f02ebc63c457dc33abc24610a93
Ian James <1732331+injms@users.noreply.github.com>
2020-01-02 17:23:07 +0000

    Remove publisher and add closing slash

--- code/layout/head.js
+++ code/layout/head.js
@@ -42,12 +42,7 @@ class Head extends React.Component {
                         return <link rel="me" href={ url } type="text/html" />
                     })
                 }
-                {
-                    this.props.rel.publisher.map( url => {
-                        return <link rel="publisher" href={ url } />
-                    })
-                }
-	 	<meta name="p:domain_verify" content="80746af448e9e3c81111669178cc05eb">
+	 	<meta name="p:domain_verify" content="80746af448e9e3c81111669178cc05eb" />
             </head>
 		);
     }
