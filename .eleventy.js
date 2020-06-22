0c4a7ab27565e6e6f937f70928688f6ebbcec99e
Ian James <injms@users.noreply.github.com>
2020-06-22 19:56:06 +0100

    🔐 Add https for local development

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const CleanCSS = require("clean-css");
 
 module.exports = (eleventyConfig) => {
@@ -5,6 +6,23 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('assets')
 
   eleventyConfig.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles)
+
+  eleventyConfig.setBrowserSyncConfig({
+    https: {
+        key: '../localhost-key.pem',
+        cert: '../localhost.pem',
+    },
+    callbacks: {
+     ready: function(err, bs) {
+       bs.addMiddleware('*', (req, res) => {
+        // Provides the 404 content without redirect.
+        res.writeHead(404)
+        res.write(fs.readFileSync('site/404/index.html'))
+        res.end()
+      })
+     }
+    }
+  })
 
   return {
     dir: {
