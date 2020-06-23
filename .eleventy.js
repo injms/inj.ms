95fdb274b2c8272573337607f66e68f2dff21ce7
Ian James <injms@users.noreply.github.com>
2020-06-23 23:16:31 +0100

    👕 Lint all the things!
     - Except the JavaScript which isn't mine...

--- .eleventy.js
+++ .eleventy.js
@@ -6,41 +6,40 @@ const formatdate = require('./nunjuck-filters/formatdate')
 const md = require('./nunjuck-filters/markdownify')
 
 module.exports = (eleventyConfig) => {
-
   eleventyConfig.addPassthroughCopy('assets')
 
   // Minifies CSS
-  eleventyConfig.addFilter("cssmin", (code) => cssmin(code) )
+  eleventyConfig.addFilter('cssmin', (code) => cssmin(code))
 
   // Passes things along to console log and returns without altering
-  eleventyConfig.addFilter('debug', (s) => debug(s) )
+  eleventyConfig.addFilter('debug', (s) => debug(s))
 
   // Display dates in reasonable formats
-  eleventyConfig.addFilter('humanDate', (date) => formatdate(date, 'human') )
-  eleventyConfig.addFilter('isoDate', (date) => formatdate(date, 'iso') )
+  eleventyConfig.addFilter('humanDate', (date) => formatdate(date, 'human'))
+  eleventyConfig.addFilter('isoDate', (date) => formatdate(date, 'iso'))
 
   // Turn a string from markdown to HTML
-  eleventyConfig.addFilter('markdownify', (markdown) => md.render(markdown) )
+  eleventyConfig.addFilter('markdownify', (markdown) => md.render(markdown))
 
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
-     ready: (_err, bs) => {
-       bs.addMiddleware('*', (_req, res) => {
-        res.writeHead(404)
+      ready: (_err, bs) => {
+        bs.addMiddleware('*', (_req, res) => {
+          res.writeHead(404)
 
-        // We read this for every 404, otherwise it'll be cached. That's
-        // faster - however we don't want to have to keep stopping and starting
-        // eleventy to see every change we make to the 404 page. Since this is
-        // for dev only, and not production, I'm okay with that.
-        res.write(fs.readFileSync('site/404/index.html'))
+          // We read this for every 404, otherwise it'll be cached. That's
+          // faster - however we don't want to have to keep stopping and starting
+          // eleventy to see every change we make to the 404 page. Since this is
+          // for dev only, and not production, I'm okay with that.
+          res.write(fs.readFileSync('site/404/index.html'))
 
-        res.end()
-      })
-     }
+          res.end()
+        })
+      },
     },
     https: {
-        key: '../localhost-key.pem',
-        cert: '../localhost.pem',
+      key: '../localhost-key.pem',
+      cert: '../localhost.pem',
     },
     ui: false,
   })
@@ -52,6 +51,6 @@ module.exports = (eleventyConfig) => {
       includes: '../_includes',
       layouts: '../_layouts',
       output: 'site',
-    }
+    },
   }
 }
